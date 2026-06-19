1fcb2a2e707573245556f5c4894e3e3d197bd06b
shifa thasnem <shifathasnemm@gmail.com>
2026-06-18 23:01:41 -0700

    Add files via upload

---
+++ merge_sorted _array.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, i, j, k;
+    int arr1[100], arr2[100], merged[200];
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter elements of first sorted array:\n");
+    for(i = 0; i < n1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements of second sorted array:\n");
+    for(i = 0; i < n2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    i = 0;
+    j = 0;
+    k = 0;
+
+    while(i < n1 && j < n2)
+    {
+        if(arr1[i] < arr2[j])
+        {
+            merged[k++] = arr1[i++];
+        }
+        else
+        {
+            merged[k++] = arr2[j++];
+        }
+    }
+
+    while(i < n1)
+    {
+        merged[k++] = arr1[i++];
+    }
+
+    while(j < n2)
+    {
+        merged[k++] = arr2[j++];
+    }
+
+    printf("Merged sorted array:\n");
+
+    for(i = 0; i < k; i++)
+    {
+        printf("%d ", merged[i]);
+    }
+
+    return 0;
+}
